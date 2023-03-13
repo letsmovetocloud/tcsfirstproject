19bd96b4d7f9db6f3e3b228e02bfa0d8754239ae
letsmovetocloud <88219893+letsmovetocloud@users.noreply.github.com>
2023-03-13 12:18:39 +0530

    azure to gcp

--- ec2.tf
+++ ec2.tf
@@ -2,14 +2,14 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/azure"
+      source  = "hashicorp/gcp"
       
     }
   }
 }
 
 # Provider Block
-provider "azure" {
+provider "gcp" {
   profile = "default" 
   region  = "ap-south-1"
 }
